758e81457c3b875c2e24713ff24a19488cbf74b6
Daniel Leite <daniel.f.m.leite@gmail.com>
2021-07-01 19:30:52 +0100

    add options and notification

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
-import './styles.css';
 import { ContextProvider } from './SocketContext';
 
+import './styles.css';
+
 ReactDOM.render(
-    <ContextProvider>
-        <App />
-    </ContextProvider>,
-    document.getElementById('root'),
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
 );
